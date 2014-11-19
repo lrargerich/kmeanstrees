98770090f1afc1fad47827024ccf360105e8e57a
Luis Argerich <lrargerich@gmail.com>
2014-11-19 04:25:52 -0300

    Horrible Bugs fixed

--- kmeans_trees_demo.py
+++ kmeans_trees_demo.py
@@ -1,13 +1,13 @@
 # A simple demo of K-Means Trees for big data with synthetic data
 # (c) 2014 by Luis Argerich
 # Any kind of educational/research use of this code is allowed
-
 from __future__ import division
 import random
 import math
 import zlib
 from pprint import *
 import csv
+import copy
 import numpy as np
 
 print "A simple demo of K-Means Trees"
@@ -16,18 +16,18 @@ print "A simple demo of K-Means Trees"
 LARGE_DISTANCE = 999999999
 
 # Number of points
-N = 50
+N = 120000
 # Number of dimensions
 dims = 2
 # The amount of points in each cluster, this is the 
 # maximum number of distances that need to be calculated
-k = 10
+k = 3
 
 # An internal count for the number of distances computed
 computed_distances = 0
 
 # Create the root of the tree
-root = {"c":[0 for _ in range(0,dims)],"n":0,"points":[]}
+root = {"c":[random.random() for _ in range(0,dims)],"n":0,"points":[]}
 
 # Online update of centroid proportional to the number of points in the cluster
 def update_centroid(centroid,point,num):
@@ -70,19 +70,29 @@ def find_k_nearest_neighbors(point,node,k):
 	# for the nearest neighbor expand (recursively)
 	# merge the results
 	heap = []
-	max_distance = 999999
+	real_points = []
+	max_distance = 99999999
+	#print "searching for:"
+	#pprint(point)
+	#print "in:"
+	#pprint(node)
+	# if the node is full then find closest centroid
 	for i in range(0,len(node['points'])):
 		new_point = node['points'][i]['c']
 		distance = compute_distance(point,new_point,max_distance)
 		if distance<max_distance or len(heap)<k:
-			heap.append((distance,new_point,i))
-			# Sort and keep the k minimum distances
-			heap = sorted(heap)
-			heap = heap[:k]
-			(max_distance,max_point,i) = heap[len(heap)-1]
-	if len(heap)>0:
-		(di,pt,best_i)=heap[0]
-		heap.extend(find_k_nearest_neighbors(point,node['points'][best_i],k))
+			if node['points'][i]['n']==0:
+				heap.append((distance,new_point,i))
+				# Sort and keep the k minimum distances
+				heap = sorted(heap)
+				heap = heap[:k]
+				(max_distance,max_point,i) = heap[len(heap)-1]
+	#print "heap:"
+	#pprint(heap)
+	if len(node['points'])>0:
+		best_centroid = find_closest_centroid(point,node['points'])
+		#print "best_centroid:"+str(best_centroid)
+		heap.extend(find_k_nearest_neighbors(point,node['points'][best_centroid],k))
 		heap = sorted(heap)
 		heap = heap[:k]
 	return heap
@@ -94,6 +104,9 @@ def find_k_nearest_neighbors(point,node,k):
 def insert(point,node):
 	# Convert the point to a node
 	node_point = {"c":point,"n":0,"points":[]}
+	if node['n']==0:
+		node['points'].append({"c":copy.deepcopy(node['c']),"n":0,"points":[]})
+		node['n']+=1
 	if node['n']<k:
 		node['points'].append(node_point)
 		node['n']+=1
@@ -106,16 +119,22 @@ def insert(point,node):
 for i in range(0,N):
 	# Create a random point
 	point = [random.random() for _ in range(0,dims)]
+	#pprint(point)
+	#if i<10:
+	#	l_points.append(copy.deepcopy(point))
 	insert(point,root)
 
 print "----------------------"
-pprint(root)
+#pprint(root)
+print "----------------------"
 print "Done!"
 print "Computed distances:"+str(computed_distances)
 
 for i in range(0,10):
 	some_point = [random.random() for _ in range(0,dims)]
+	#some_point = l_points[i]
 	print "Now searching for:"
 	pprint(some_point)
+	print "========="
 	l = find_k_nearest_neighbors(some_point,root,3)
 	pprint(l)
